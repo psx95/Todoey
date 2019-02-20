9e54687aabd64004041c25a238e77a02f53cea26
psx95 <pranav.ps95@hotmail.com>
2019-02-21 00:18:33 +0530

    refactor(AppDelegate): Removed the CoreData code

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -43,50 +43,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         Realm.Configuration.defaultConfiguration = config
         
-        
-        lazy var _: Realm = {
-            return try! Realm()
-        }()
-//        do {
-//            let _ = try! Realm()
-//        } catch {
-//            print("Unable to initialise Realm \(error)")
-//        }
+        do {
+            _ = try Realm()
+        } catch {
+            print("Unable to initialise Realm \(error)")
+        }
         
         return true
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
-    }
-    
-    // MARK: - Core Data stack
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-        
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-            
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
     }
 
 }
